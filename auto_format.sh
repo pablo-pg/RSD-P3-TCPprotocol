658401a1eccb1fe4d9c4b011ba9af732c6c54b91
Pablo <alu0101318318@ull.edu.es>
2021-05-14 13:20:50 +0100

    Plantilla formateada

---
+++ auto_format.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Aplica formato a todos los ficheros de código fuente C++,
+# según lo establecido en un archivo de configuración.
+
+# Guía de estilos predeterminada para archivos de configuración,
+STYLE=google
+
+# Revisa si clang-format está instalado en el sistema.
+if [ "$(which clang-format)" != "" ]; then
+  # Busca un fichero de configuración.
+  if [ "$(find . -maxdepth 1 -type f -name ".clang-format")" = "" ]; then
+    echo "No se encuentra un fichero de configuracion"
+    echo -n "Desea crear uno (estilo=${STYLE})? (s/n) > "
+    read generate
+
+    # Pregunta al usuario sobre la creación de un fichero básico.
+    if [ "$generate" = "s" ] || [ "$generate" = "S" ]; then
+      clang-format -style=$STYLE -dump-config > .clang-format
+    else
+      echo "Obligatorio: crea un fichero de configuracion"
+      exit 1
+    fi
+  fi
+  
+  # Aplica formato sobre los ficheros .h en la carpeta include.
+  if clang-format -i -style=file src/*.h; then
+    echo "Include: se ha formateado el codigo!!!"
+  fi
+  
+  # Aplica formato sobre los ficheros .cc en la carpeta src.
+  if clang-format -i -style=file src/*.cpp; then
+    echo "Src: se ha formateado el codigo!!!"
+  fi
+
+else
+  echo "Obligatorio: sudo apt install clang-format"
+  exit 1
+fi
---
+++ src/FTPServer.cpp
@@ -0,0 +1,68 @@
+//****************************************************************************
+//                         REDES Y SISTEMAS DISTRIBUIDOS
+//
+//                     2º de grado de Ingeniería Informática
+//
+//                        Main class of the FTP server
+//
+//****************************************************************************
+
+#include "FTPServer.h"
+
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <cerrno>
+#include <cstdarg>
+#include <cstdio>
+#include <cstring>
+#include <list>
+
+#include "ClientConnection.h"
+#include "common.h"
+
+int define_socket_TCP(int port) {
+  // Include the code for defining the socket.
+
+  return -1;
+}
+
+// This function is executed when the thread is executed.
+void *run_client_connection(void *c) {
+  ClientConnection *connection = (ClientConnection *)c;
+  connection->WaitForRequests();
+
+  return NULL;
+}
+
+FTPServer::FTPServer(int port) { this->port = port; }
+
+// Parada del servidor.
+void FTPServer::stop() {
+  close(msock);
+  shutdown(msock, SHUT_RDWR);
+}
+
+// Starting of the server
+void FTPServer::run() {
+  struct sockaddr_in fsin;
+  int ssock;
+  socklen_t alen = sizeof(fsin);
+  msock = define_socket_TCP(port);  // This function must be implemented by you.
+  while (1) {
+    pthread_t thread;
+    ssock = accept(msock, (struct sockaddr *)&fsin, &alen);
+    if (ssock < 0) errexit("Fallo en el accept: %s\n", strerror(errno));
+
+    ClientConnection *connection = new ClientConnection(ssock);
+
+    // Here a thread is created in order to process multiple
+    // requests simultaneously
+    pthread_create(&thread, NULL, run_client_connection, (void *)connection);
+  }
+}
